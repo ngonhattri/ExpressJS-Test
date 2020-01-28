75b81713a96a54541d709c28a9337954263cac36
minhquan3197 <minhquan.k0r1@gmail.com>
2020-01-28 17:31:46 +0700

    Update template

--- src/server.js
+++ src/server.js
@@ -1,27 +1,20 @@
 import express from 'express';
 import ConnectDB from './config/connect_database';
-import UserModel from './models/user.model';
-import BlogCategoryModel from './models/blog_category.model';
-import BlogModel from './models/blog.model';
+
+import configViewEngine from "./config/view_engine";
 let app = express();
-
+require('dotenv').config();
 ConnectDB();
+configViewEngine(app);
 
-let hostname = 'localhost';
-let port = 3000;
-
-app.get('/', async (req, res) => {
-    try {
-        let item = {
-            title: 'test'
-        };
-        let contact = await BlogCategoryModel.createNew(item);
-        res.send(contact);
-    } catch (err) {
-        console.log(err);
-    }
+app.get("/", (req, res) => {
+    return res.render("main/home/home");
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Running at ${hostname}:${port}`);
+app.get("/login", (req, res) => {
+    return res.render("auth/login");
+});
+
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+    console.log(`Server listenning at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
 });
