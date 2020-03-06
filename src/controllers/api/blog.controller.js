1b60b581a578b3c729e4e5b36bf11f64a1507890
minhquan3197 <minhquan.k0r1@gmail.com>
2020-03-07 00:24:55 +0700

    Fix bug blog

--- src/controllers/api/blog.controller.js
+++ src/controllers/api/blog.controller.js
@@ -5,17 +5,30 @@ let getBlogs = async (req, res) => {
     const page = Number(req.query.page) || 1;
     req.query.select = 'name image createdAt';
     req.query.status = true;
-    const foundProducts = await blog.getPaginateBlog(resPerPage, req.query);
-    const numOfResults = await blog.getCountBlog(req.query);
-    return res.status(200).json({
-        message: 'oK',
-        data: {
-            products: foundProducts,
-            currentPage: page,
-            pages: Math.ceil(numOfResults / resPerPage),
-            numOfResults
-        }
-    });
+    try {
+        const foundProducts = await blog.getPaginateBlog(resPerPage, req.query);
+        const numOfResults = await blog.getCountBlog(req.query);
+        return res.status(200).json({
+            message: 'oK',
+            data: {
+                products: foundProducts,
+                currentPage: page,
+                pages: Math.ceil(numOfResults / resPerPage),
+                numOfResults
+            }
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            message: 'oK',
+            data: {
+                products: [],
+                currentPage: 0,
+                pages: 0,
+                numOfResults: 0
+            }
+        });
+    }
 };
 
 let detailBlogs = async (req, res) => {
